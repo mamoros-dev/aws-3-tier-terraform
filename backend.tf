69608766a8a7e3d883a8a5c951c95783b3bd6efa
Miguel Amorós Moret <miguel14amoros@gmail.com>
2026-08-03 22:20:40 +0200

    chore: configuracion backend remoto s3+dynamoDB y estructura inicial del proyecto

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "miguel-terraform-state-proyecto2"
+    key            = "proyecto2/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks-proyecto2"
+    encrypt        = true
+    profile        = "personal"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "Región de AWS donde se despliega toda la infraestructura"
+  type        = string
+  default     = "eu-west-1"
+}
